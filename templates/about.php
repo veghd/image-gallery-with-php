7d5c7aa305f0cd4b710dff34e9b34e01d7111e3f
veghd <60345432+veghd@users.noreply.github.com>
2020-02-06 12:59:53 +0100

    about

---
+++ templates/about.php
@@ -0,0 +1,10 @@
+<main class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">About</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Expedita porro, architecto laboriosam numquam nam perspiciatis explicabo sint non dolores 
+            tempore obcaecati tenetur saepe quasi debitis unde labore cum vel? Iusto?</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="/" role="button">Home</a>
+    </div>
+</main>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,4 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a href="#" class="navbar-brand">My Photos</a>
+    <a href="about.php" class="navbar-brand">About</a>
 </nav>
